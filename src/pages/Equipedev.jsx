e9486fafcdb14188676a7a9722153cb28aea72d2
EUDANTAS RODRIGUES ALVES <eudantas.ralves@senacsp.edu.br>
2025-06-12 20:06:03 -0300

    equipe de desenvolvimento

--- src/pages/Equipedev.jsx
+++ src/pages/Equipedev.jsx
@@ -5,10 +5,12 @@ import { FaEnvelope, FaPhoneAlt, FaMapMarkerAlt } from 'react-icons/fa';
 const Equipedev = () => {
   return (
     <div className="container">
-      <h1>Projeto Integrador Opina +</h1>
-      <p>O Projeto Integrador Opina + , lê-se "opina mais" é o projeto de encerramento do curso Técnico de Informática do Senac Campinas em junho de 2025.</p>
-      <h3>Equipe</h3>
-      <p>Eudantas,  Douglas, Simone e Felipe.</p>
+      <div>
+        <h1>Projeto Integrador Opina +</h1>
+        <p>O Projeto Integrador Opina + , lê-se "opina mais" é o projeto de encerramento do curso Técnico de Informática do Senac Campinas em junho de 2025.</p>
+        <h3>Equipe</h3>
+        <p>Eudantas,  Douglas, Simone e Felipe.</p>
+      </div>
     </div>
   );
 };
