4502307ce74c29d44914532fce8af2a0cda9443a
Eudantas <eudantas.alves@gmail.com>
2025-06-12 23:40:05 -0300

    integrador

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,48 +16,46 @@ const Header = ({ userType, onLogout }) => {
   };
 
   return (
-    <nav className="main-header">
-  {/* Bloco da Esquerda - Logo */}
-  <div className="header-left">
-    <Link to="/" className="logo-link">
-      <img className="logoopina" src="./logo6.svg" alt="Opina+ Logo" />
-    </Link>
-  </div>
+    <nav className="main-header" aria-label="Main navigation">
+      <div className="header-left">
+        <Link to="/" className="logo-link" aria-label="Página inicial">
+          <img className="logoopina" src="./logo6.svg" alt="Opina+ Logo" />
+        </Link>
+      </div>
 
-  {/* Bloco Central - Busca */}
-  {/* <div className="header-center">
-    <BuscaEmpresa />
-  </div> */}
+      {/* Descomente para ativar a busca */}
+      {/* <div className="header-center">
+        <BuscaEmpresa />
+      </div> */}
 
-  {/* Bloco da Direita - Navegação */}
-  <div className="header-right">
-    <Link to="/" className="nav-link"><FaHome /> Início</Link>
-    <Link to="/blog" className="nav-link"><FaBook /> Quem Somos</Link>
-    <Link to="/reclamacoes" className="nav-link"><FaBullhorn /> Feedbacks</Link>
+      <div className="header-right">
+        <Link to="/" className="nav-link"><FaHome aria-hidden="true" /> Início</Link>
+        <Link to="/blog" className="nav-link"><FaBook aria-hidden="true" /> Quem Somos</Link>
+        <Link to="/reclamacoes" className="nav-link"><FaBullhorn aria-hidden="true" /> Feedbacks</Link>
 
-    {!userType ? (
-      <>
-        <Link to="/login" className="nav-link"><FaSignInAlt /> Login</Link>
-        <Link to="/cadastro" className="nav-link"><FaUserPlus /> Cadastro</Link>
-        
-      </>
-    ) : (
-      <>
-        {userType === 'cliente' && (
+        {!userType ? (
           <>
-            <Link to="/novo-feedback" className="nav-link"><FaPlusCircle /> Criar novo</Link>
-            <Link to="/meus-feedbacks" className="nav-link"><FaCommentDots /> Minha Área</Link>
+            <Link to="/login" className="nav-link"><FaSignInAlt aria-hidden="true" /> Login</Link>
+            <Link to="/cadastro" className="nav-link"><FaUserPlus aria-hidden="true" /> Cadastro</Link>
+          </>
+        ) : (
+          <>
+            {userType === 'cliente' && (
+              <>
+                <Link to="/novo-feedback" className="nav-link"><FaPlusCircle aria-hidden="true" /> Criar novo</Link>
+                <Link to="/meus-feedbacks" className="nav-link"><FaCommentDots aria-hidden="true" /> Minha Área</Link>
+              </>
+            )}
+            {userType === 'empresa' && (
+              <Link to="/painel-empresa" className="nav-link"><FaChartBar aria-hidden="true" /> Painel da Empresa</Link>
+            )}
+            <button type="button" onClick={handleLogoutClick} className="nav-button" aria-label="Sair da aplicação">
+              <FaSignOutAlt aria-hidden="true" /> Sair
+            </button>
           </>
         )}
-        {userType === 'empresa' && (
-          <Link to="/painel-empresa" className="nav-link"><FaChartBar /> Painel da Empresa</Link>
-        )}
-        <button onClick={handleLogoutClick} className="nav-button"><FaSignOutAlt /> Sair</button>
-      </>
-    )}
-  </div>
-</nav>
-
+      </div>
+    </nav>
   );
 };
 
